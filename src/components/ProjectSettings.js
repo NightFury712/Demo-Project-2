32044ee8b94ac967033f2e7cf3bf95b4469e64c7
datbt183880 <dat.bt183880@sis.hust.edu.vn>
2021-05-21 09:19:17 +0700

    push code project setting

--- src/components/ProjectSettings.js
+++ src/components/ProjectSettings.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import HeaderProject from '../items/HeaderProject'
+import '../css/ProjectSettings.css'
 
 const ProjectSettings = () => {
-  return (
-    <React.Fragment>
-      <h1>Hello</h1>
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<div className="full-settings">
+				<HeaderProject/>
+
+				<div className="content-wrapper">
+					<article>
+						<ul className="setting-nav">
+							<li className="generals-menu">
+								<h2 className="settings-nav-title">Project Settings</h2>
+								
+								<ul className="setting-nav-menu">
+									<li className="is_active">
+										<a href="/projectSettings/general">General</a>
+									</li>
+									<li className="is_active">
+										<a href="/projectSettings/members">Members</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</article>
+					<aside>
+						ss
+					</aside>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 }
 
 export default ProjectSettings;
