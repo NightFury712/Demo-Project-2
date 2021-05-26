ece8e70f3192f6d98b2e29cde3771e72dc1715f1
chienit3bk <chienit3bk@gmail.com>
2021-05-26 09:52:50 +0700

    Chien add FromLogin

---
+++ src/FormLogin/FormSuccess.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './FormLogin.css';
+
+const FormSuccess = () => {
+    return (
+        <div className="form-content-right">
+            <div className="form-success">
+                We have received your request!
+            </div>
+            <img src='img/img-3.svg' alt='success-image' className="form-img-2"/>
+        </div>
+    );
+}
+
+export default FormSuccess;
---
+++ src/FormLogin/useForm.js
@@ -0,0 +1,36 @@
+import{useState, useEffect} from 'react'
+
+const useForm = (callback, validate) => {
+    const [values, setvalues] = useState({
+        username: '',
+        email: '',
+        password: '',
+        password2: ''
+    })
+    const [errors, setErrors] = useState({})
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleChange = e => {
+        const {name, value} = e.target
+        setvalues({
+            ...values,
+            [name]: value
+        });
+    };
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        setErrors(validate(values));
+    };
+
+    useEffect(() => {
+            if(Object.keys(errors).length === 0 && isSubmitting) {
+                callback();
+            }
+        },
+        [errors]
+    );
+    return { handleChange, values, handleSubmit, errors };
+};
+
+export default useForm;
